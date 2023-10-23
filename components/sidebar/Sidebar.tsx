0ef491d55cf69e2242e35dc6feba31a637356644
ayakuma309 <ayako.30901@gmail.com>
2023-10-23 22:11:04 +0900

    💄 fix: avatar ui sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { User } from '@prisma/client';
 import Menu from './Menu';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 type SidebarProps = {
   currentUser: User | null;
@@ -12,7 +13,12 @@ const Sidebar: React.FC<SidebarProps> = ({ currentUser }) => {
   return (
     <>
       <div className='w-20 sm:block fixed h-screen duration-300 bg-gray-100 p-3 dark:bg-slate-800 shadow-lg'>
-        {/* <Avatar img='/ogp.png' /> */}
+        <div className='flex justify-center'>
+          <Avatar>
+            <AvatarImage src='/ogp.png' />
+            <AvatarFallback>Logo</AvatarFallback>
+          </Avatar>
+        </div>
         <Menu currentUser={currentUser} />
       </div>
     </>
