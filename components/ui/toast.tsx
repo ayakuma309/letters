6c773ef843c29fec890c2e5ce8c1107dbc366912
ayakuma309 <ayako.30901@gmail.com>
2023-10-23 23:13:10 +0900

    💄 add: card ui

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -30,6 +30,7 @@ const toastVariants = cva(
         default: 'border bg-background text-foreground',
         destructive:
           'destructive group border-destructive bg-destructive text-destructive-foreground',
+        success: 'bg-green-500 text-white',
       },
     },
     defaultVariants: {
