700301cf20e7e1f3dffd38375479ecf911968790
ayakuma309 <ayako.30901@gmail.com>
2023-10-25 18:01:24 +0900

    ✨ add: NextAuth の設定 (Googleログインのみにする。)
    後で必要であれば追加する

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
-import bcrypt from 'bcrypt';
 import prisma from '@/lib/prisma';
 
 // NextAuth設定
@@ -16,45 +14,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    // メールアドレス認証
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        // メールアドレスとパスワード
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' },
-      },
-      async authorize(credentials) {
-        // メールアドレスとパスワードがない場合はエラー
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('メールアドレスとパスワードが存在しません');
-        }
-
-        // ユーザーを取得
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-
-        // ユーザーが存在しない場合はエラー
-        if (!user || !user?.hashedPassword) {
-          throw new Error('ユーザーが存在しません');
-        }
-
-        // パスワードが一致しない場合はエラー
-        const isCorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
-
-        if (!isCorrectPassword) {
-          throw new Error('パスワードが一致しません');
-        }
-
-        return user;
-      },
     }),
   ],
   session: {
