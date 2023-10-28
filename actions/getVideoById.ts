43a3066bf059535666f824f3b9999fb3bd242678
ayakuma309 <ayako.30901@gmail.com>
2023-10-28 18:29:37 +0900

    ✨ feat: video 詳細へのリンク　

---
+++ actions/getVideoById.ts
@@ -0,0 +1,22 @@
+import prisma from '@/lib/prisma';
+
+const getVideo = async ({ id }: { id: string }) => {
+  try {
+    const video = await prisma.youTube.findUnique({
+      where: { id: parseInt(id) },
+      include: {
+        tags: true,
+      },
+    });
+
+    if (!video) {
+      return null;
+    }
+    return video;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export default getVideo;
