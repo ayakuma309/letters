ec488b95d19aecbbfeb36688fd8b0c9e3b4eb698
ayakuma309 <ayako.30901@gmail.com>
2023-10-29 10:58:05 +0900

    ✨ feat: 保存したqiita記事の表示

--- app/qiitas/page.tsx
+++ app/qiitas/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Metadata } from 'next';
 import getQiita from '@/actions/getQiita';
+import QiitaArticle from '@/components/qiitas/QiitaArticle';
 
 export const metadata: Metadata = {
   title: 'Qiita記事',
@@ -9,6 +10,20 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   const qiitas = await getQiita();
-  console.log(qiitas);
-  return <div className='space-y-2'></div>;
+  return (
+    <div className='space-y-2 mx-auto'>
+      {qiitas &&
+        qiitas.map((qiita) => (
+          // <QiitaArticle key={qiita.id} {...qiita} />
+          <QiitaArticle
+            key={qiita.id}
+            id={qiita.id}
+            url={qiita.url}
+            title={qiita.title}
+            tags={qiita.tags}
+            profileImageUrl={qiita.profileImageUrl}
+          />
+        ))}
+    </div>
+  );
 }
