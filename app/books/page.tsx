67b0c11f30e82b987abb7ffab22960373f2163e7
ayakuma309 <ayako.30901@gmail.com>
2023-10-29 21:12:49 +0900

    ✨ feat: book一覧の表示

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Metadata } from 'next';
+import getBook from '@/actions/getBook';
+import Book from '@/components/books/Book';
+import { BookType } from '@/types/types';
 
 export const metadata: Metadata = {
   title: '書籍情報',
@@ -7,5 +10,21 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  return <div className='space-y-2 mx-auto'></div>;
+  const books = await getBook();
+
+  if (books.length === 0) {
+    return <div className='text-center'>投稿はありません</div>;
+  }
+
+  return (
+    <div className='space-y-2'>
+      {books && (
+        <div className='flex flex-wrap justify-center'>
+          {books.map((book: BookType) => (
+            <Book key={book.id} book={book} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
---
+++ components/books/Book.tsx
@@ -0,0 +1,55 @@
+'use client';
+import { BookType } from '@/types/types';
+import { useSession } from 'next-auth/react';
+
+import React from 'react';
+import { BsFillTrashFill } from 'react-icons/bs';
+import { toast } from '../ui/use-toast';
+import axios from 'axios';
+import Link from 'next/link';
+
+type BookProps = {
+  book: BookType;
+};
+const Book: React.FC<BookProps> = ({ book }) => {
+  const { data: session } = useSession();
+  const handleDelete = async (id: number) => {
+    const shouldDelete = window.confirm('この投稿を削除しますか？');
+    if (!shouldDelete) return;
+    try {
+      await axios.delete(`/api/book/${id}`);
+      toast({
+        title: '削除しました',
+        variant: 'success',
+      });
+    } catch (err) {
+      toast({
+        title: '削除に失敗しました',
+        variant: 'destructive',
+      });
+    }
+  };
+  return (
+    <div className='m-1 py-3 px-8  rounded-lg shadow-lg'>
+      <div className='flex flex-col items-center mt-3 justify-center'>
+        <Link href={book.infoLink} rel='noopener noreferrer' target='_blank'>
+          <img
+            className='w-100 h-100 rounded mr-4'
+            alt='User Avatar'
+            src={book.image}
+          />
+        </Link>
+      </div>
+      {session?.user && (
+        <button
+          className='p-1 rounded-md font-bold text-2xl'
+          onClick={() => handleDelete(book.id)}
+        >
+          <BsFillTrashFill />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Book;
