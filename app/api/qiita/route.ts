93b44d643217d92fc167ba7e0c701ff20b52d441
ayakuma309 <ayako.30901@gmail.com>
2023-10-29 10:23:16 +0900

    ✨ feat: qiita new action

---
+++ app/api/qiita/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import getCurrentUser from '@/actions/getCurrentUser';
+import createQiita from '@/actions/createQiita';
+
+// 新規投稿
+export async function POST(request: Request) {
+  try {
+    // リクエストボディの取得
+    const body = await request.json();
+    const { qiitaId, title, url, profileImageUrl, tags } = body;
+
+    // ログインユーザーの取得
+    const currentUser = await getCurrentUser();
+
+    // ログインしていない場合はエラー
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse('認証していません', { status: 401 });
+    }
+
+    // 新規投稿
+    const response = await createQiita({
+      qiitaId,
+      title,
+      url,
+      profileImageUrl,
+      tags,
+    });
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse('Error', { status: 500 });
+  }
+}
