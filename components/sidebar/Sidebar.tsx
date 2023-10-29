28d0b0d5fd4ad9178145663797c0195aa0fc9585
ayakuma309 <ayako.30901@gmail.com>
2023-10-29 10:57:36 +0900

    💄 fix: sidebar修正

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -11,17 +11,15 @@ type SidebarProps = {
 
 const Sidebar: React.FC<SidebarProps> = ({ currentUser }) => {
   return (
-    <>
-      <div className='w-20 sm:block fixed h-screen duration-300 bg-gray-100 p-3 dark:bg-slate-800 shadow-lg'>
-        <div className='flex justify-center'>
-          <Avatar>
-            <AvatarImage src='/ogp.png' />
-            <AvatarFallback></AvatarFallback>
-          </Avatar>
-        </div>
-        <Menu currentUser={currentUser} />
+    <div className='w-20 sm:block fixed h-screen duration-300 bg-gray-100 p-3 dark:bg-slate-800 shadow-lg'>
+      <div className='flex justify-center'>
+        <Avatar>
+          <AvatarImage src='/ogp.png' />
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
       </div>
-    </>
+      <Menu currentUser={currentUser} />
+    </div>
   );
 };
 
