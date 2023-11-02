e07380cd1da4062fe513a4ac20e956100b489083
ayakuma309 <ayako.30901@gmail.com>
2023-11-02 12:00:30 +0900

    🚚

--- app/_components/ui/toaster.tsx
+++ app/_components/ui/toaster.tsx
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from '@/components/ui/toast';
-import { useToast } from '@/components/ui/use-toast';
+} from '@/app/_components/ui/toast';
+import { useToast } from '@/app/_components/ui/use-toast';
 
 export function Toaster() {
   const { toasts } = useToast();
