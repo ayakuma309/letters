5c13e95df0b78e5ce3e2b86b8ebc2ff3c4e36f33
ayakuma309 <ayako.30901@gmail.com>
2023-11-03 17:09:09 +0900

    ✨ お問い合わせ #11

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import getCurrentUser from '@/actions/getCurrentUser';
 import Sidebar from '@/app/_components/sidebar/Sidebar';
 
 import { Toaster } from '@/app/_components/ui/toaster';
+import MenuItem from './_components/sidebar/MenuItem';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -37,6 +38,20 @@ export default async function RootLayout({
               {/* フッター */}
               <footer className='py-5'>
                 <div className='text-center text-sm'>
+                  <div className='flex justify-center space-x-5'>
+                    <a
+                      href='https://twitter.com/ZCunkuma'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      <MenuItem
+                        title={'お問い合わせはこちらからお願いします'}
+                        src={
+                          <img src='https://img.icons8.com/fluent/30/000000/twitter.png' />
+                        }
+                      />
+                    </a>
+                  </div>
                   Copyright © All rights reserved | Letters Tube
                 </div>
               </footer>
