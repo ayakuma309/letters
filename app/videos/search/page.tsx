8d182d0e4cd8630e25f45dffe2edcb14228a12b5
ayakuma309 <ayako.30901@gmail.com>
2023-11-03 16:19:06 +0900

    🛂 admin対応 #39

--- app/videos/search/page.tsx
+++ app/videos/search/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { redirect } from 'next/navigation';
 import getCurrentUser from '@/actions/getCurrentUser';
 import Search from '@/app/videos/search/_components/Search';
+import { Role } from '@prisma/client';
 
 export const metadata: Metadata = {
   title: 'YouTube',
@@ -13,7 +14,7 @@ export default async function Page() {
   // 認証情報取得
   const session = await getCurrentUser();
   // ログインしていない場合はログイン画面にリダイレクト
-  if (!session) {
+  if (!session || session.role !== Role.ADMIN) {
     redirect('/login');
   }
   return (
