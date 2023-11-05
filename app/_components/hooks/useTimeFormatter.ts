e3a4f4b6e11a52992de983a8ec60590ea84135e4
ayakuma309 <ayako.30901@gmail.com>
2023-11-05 11:42:15 +0900

    ⚡️

--- app/_components/hooks/useTimeFormatter.ts
+++ app/_components/hooks/useTimeFormatter.ts
@@ -1,18 +1,22 @@
+import { useMemo } from 'react';
+
 const useTimeFormatter = (secValue: number) => {
-  const secInt = parseInt(secValue.toString(), 10);
-  const hours = Math.floor(secInt / 3600);
-  const minutes = Math.floor((secInt - hours * 3600) / 60);
-  const seconds = secInt - hours * 3600 - minutes * 60;
+  return useMemo(() => {
+    const secInt = parseInt(secValue.toString(), 10);
+    const hours = Math.floor(secInt / 3600);
+    const minutes = Math.floor((secInt - hours * 3600) / 60);
+    const seconds = secInt - hours * 3600 - minutes * 60;
 
-  const formattedHours = hours < 10 ? '0' + hours : hours;
-  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-  const formattedSecounds = seconds < 10 ? '0' + seconds : seconds;
+    const formattedHours = hours < 10 ? '0' + hours : hours;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    const formattedSecounds = seconds < 10 ? '0' + seconds : seconds;
 
-  if (formattedHours == '00') {
-    return formattedMinutes + ':' + formattedSecounds;
-  }
+    if (formattedHours == '00') {
+      return formattedMinutes + ':' + formattedSecounds;
+    }
 
-  return formattedHours + ':' + formattedMinutes + ':' + formattedSecounds;
+    return formattedHours + ':' + formattedMinutes + ':' + formattedSecounds;
+  }, [secValue]);
 };
 
 export default useTimeFormatter;
