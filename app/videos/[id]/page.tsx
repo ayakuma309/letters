677f93f4f660d251078994f6dd52818646a29447
ayakuma309 <ayako.30901@gmail.com>
2023-11-11 14:46:48 +0900

    🎨 fix #58

--- app/videos/[id]/page.tsx
+++ app/videos/[id]/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
-import getVideoById from '@/actions/getVideoById';
-import getBook from '@/actions/getBook';
-import getVideoBook from '@/actions/getVideoBook';
-import VideoItem from './_components/VideoItem';
+import VideoDetail from './_components/VideoDetail';
+import VideoBooks from './_components/books/VideoBooks';
+import { Suspense } from 'react';
+import Spinner from '@/app/_components/ui/spinner';
 
 export const metadata: Metadata = {
   title: 'YouTube',
@@ -16,21 +16,14 @@ type Props = {
 };
 
 export default async function Page({ params: { id } }: Props) {
-  const [video, books, videoBooks] = await Promise.all([
-    getVideoById(id),
-    getBook(),
-    getVideoBook(id),
-  ]);
-  if (!video || !books) {
-    return (
-      <div className='text-center'>
-        {video ? '書籍はありません' : '動画はありません'}
-      </div>
-    );
-  }
   return (
     <div className='sm:ml-20 mx-auto mt-10'>
-      <VideoItem video={video} books={books} videoBooks={videoBooks} />
+      <Suspense fallback={<Spinner />}>
+        <VideoDetail videoId={id} />
+      </Suspense>
+      <Suspense fallback={<Spinner />}>
+        <VideoBooks videoId={id} />
+      </Suspense>
     </div>
   );
 }
