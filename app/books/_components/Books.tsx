447b9a69f2b786a5cae3dd47ee18c62b83bd03e2
ayakuma309 <ayako.30901@gmail.com>
2024-02-26 14:07:08 +0900

    fix:

--- app/books/_components/Books.tsx
+++ app/books/_components/Books.tsx
@@ -2,24 +2,18 @@
 import React from 'react';
 import Book from './Book';
 import { BookType } from '@/types/types';
-import { bookTags } from '@/constants/bookTag';
-import TagList from '@/app/_components/common/search/TagList';
-import { useBookTagSearch } from '@/app/_components/hooks/useTagSearch';
-import SearchBar from '@/app/_components/common/search/SearchBar';
 
 type Props = {
   books: BookType[];
 };
 
 export default function Books({ books }: Props) {
-  const { searchResults, handleSearch } = useBookTagSearch(books);
   return (
     <>
-      <SearchBar />
-      <TagList handleSearch={handleSearch} tagOptions={bookTags} />
       <div className='flex flex-wrap items-center justify-center'>
-        {searchResults &&
-          searchResults.map((book) => <Book key={book.id} book={book} />)}
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
       </div>
     </>
   );
