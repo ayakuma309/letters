9927542a699fdd3491b82c74d94aa24d58486a5a
ayakuma309 <ayako.30901@gmail.com>
2024-03-03 00:54:34 +0900

    add: noteをさらに見るを表示

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,11 +1,12 @@
 import { getNoteRssFeeds } from '@/actions/get-note-feed';
 import NotePost from './_components/NotePost';
+import Link from 'next/link';
 
 export default async function Page() {
   const notes = await getNoteRssFeeds();
   return (
     <div className='mt-5 flex sm:pl-24'>
-      {notes.map(({ pagePosts, title }, i) => (
+      {notes.map(({ link, pagePosts, title }, i) => (
         <div
           key={i}
           className='mr-1 h-screen overflow-scroll p-3 min-w-[450px]'
@@ -14,6 +15,9 @@ export default async function Page() {
           {pagePosts.map((post, i) => (
             <NotePost key={i} post={post} />
           ))}
+          <Link href={link as string} rel='noopener noreferrer' target='_blank'>
+            さらに見る
+          </Link>
         </div>
       ))}
     </div>
