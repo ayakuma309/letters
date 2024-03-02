ff7b097a2cc989b482bff489d9bc4df1d7bd6273
ayakuma309 <ayako.30901@gmail.com>
2024-03-02 23:09:47 +0900

    fix:

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -4,7 +4,7 @@ import NotePost from './_components/NotePost';
 export default async function Page() {
   const notes = await getNoteRssFeeds();
   return (
-    <div className=' bg-red-700 flex flex-col'>
+    <div className='bg-red-700 flex flex-col'>
       <div className='mt-5 flex'>
         {notes.map(({ pagePosts, title }, i) => (
           <div key={i} className='mr-1 h-screen overflow-scroll p-3'>
--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,51 +1,53 @@
 export default function PrivacyPolicy() {
   return (
-    <div className='flex flex-1 mx-auto'>
-      <div className='mx-8 md:mx-20 py-5  mt-12'>
-        <article className='prose prose-sm max-w-none'>
-          <p>
-            <span className='font-bold'>Tube Letters</span>
-            （以下、「当方」といいます。）は、本ウェブサイト上で提供するサービス（以下、「本サービス」といいます。）におけるユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
-          </p>
-        </article>
-        <hr className='h-px my-8 bg-gray-300 border-0 ' />
-        <article className='prose prose-sm'>
-          <h3 className='font-bold'>アクセス解析ツール</h3>
-          <p>
-            当方は、お客様のアクセス解析のために、「Googleアナリティクス」を利用しています。Googleアナリティクスは、トラフィックデータの収集のためにCookieを使用しています。トラフィックデータは匿名で収集されており、個人を特定するものではありません。Cookieを無効にすれば、これらの情報の収集を拒否することができます。詳しくはお使いのブラウザの設定をご確認ください。
-            Googleアナリティクスについて、詳しくは
-            <a
-              href='https://marketingplatform.google.com/about/analytics/terms/jp/'
-              target='_blank'
-              rel='noreferrer'
-              className='underline'
-            >
-              こちら
-            </a>
-            からご確認ください。
-          </p>
-          <h3 className='font-bold'>プライバシーポリシーの変更</h3>
-          <p>
-            当方は、必要に応じて、このプライバシーポリシーの内容を変更します。この場合、変更後のプライバシーポリシーの施行時期と内容を適切な方法により周知または通知します。
-          </p>
-          <h3 className='font-bold'>お問い合わせ</h3>
-          <p>
-            お客様の情報の開示、情報の訂正、利用停止、削除をご希望の場合は、以下のTwitterアカウントにご連絡ください。
-          </p>
-          <span>
-            <a
-              href='https://twitter.com/ZCunkuma'
-              target='_blank'
-              rel='noreferrer'
-              className='underline'
-            >
-              Twitterアカウント
-            </a>
-          </span>
-          <br></br>
-          <p>以上</p>
-          <p>2023年06月18日 制定</p>
-        </article>
+    <div className='container mx-auto max-w-screen-md px-2 min-h-screen'>
+      <div className='flex flex-1 mx-auto'>
+        <div className='mx-8 md:mx-20 py-5  mt-12'>
+          <article className='prose prose-sm max-w-none'>
+            <p>
+              <span className='font-bold'>Tube Letters</span>
+              （以下、「当方」といいます。）は、本ウェブサイト上で提供するサービス（以下、「本サービス」といいます。）におけるユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
+            </p>
+          </article>
+          <hr className='h-px my-8 bg-gray-300 border-0 ' />
+          <article className='prose prose-sm'>
+            <h3 className='font-bold'>アクセス解析ツール</h3>
+            <p>
+              当方は、お客様のアクセス解析のために、「Googleアナリティクス」を利用しています。Googleアナリティクスは、トラフィックデータの収集のためにCookieを使用しています。トラフィックデータは匿名で収集されており、個人を特定するものではありません。Cookieを無効にすれば、これらの情報の収集を拒否することができます。詳しくはお使いのブラウザの設定をご確認ください。
+              Googleアナリティクスについて、詳しくは
+              <a
+                href='https://marketingplatform.google.com/about/analytics/terms/jp/'
+                target='_blank'
+                rel='noreferrer'
+                className='underline'
+              >
+                こちら
+              </a>
+              からご確認ください。
+            </p>
+            <h3 className='font-bold'>プライバシーポリシーの変更</h3>
+            <p>
+              当方は、必要に応じて、このプライバシーポリシーの内容を変更します。この場合、変更後のプライバシーポリシーの施行時期と内容を適切な方法により周知または通知します。
+            </p>
+            <h3 className='font-bold'>お問い合わせ</h3>
+            <p>
+              お客様の情報の開示、情報の訂正、利用停止、削除をご希望の場合は、以下のTwitterアカウントにご連絡ください。
+            </p>
+            <span>
+              <a
+                href='https://twitter.com/ZCunkuma'
+                target='_blank'
+                rel='noreferrer'
+                className='underline'
+              >
+                Twitterアカウント
+              </a>
+            </span>
+            <br></br>
+            <p>以上</p>
+            <p>2023年06月18日 制定</p>
+          </article>
+        </div>
       </div>
     </div>
   );
