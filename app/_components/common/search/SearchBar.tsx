cf0bbff9e205aeb0347379ea2bfaf109f4562a08
ayakuma309 <ayako.30901@gmail.com>
2024-03-02 18:39:23 +0900

    add: 検索機能

--- app/_components/common/search/SearchBar.tsx
+++ app/_components/common/search/SearchBar.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+
 type Props = {
+  handleSearch: (tag: string) => void;
   bgColor?: string;
 };
 
-export default function SearchBar({ bgColor = 'bg-red-600' }: Props) {
+export default function SearchBar({
+  bgColor = 'bg-red-600',
+  handleSearch,
+}: Props) {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleClick = () => {
+    handleSearch(title);
+  };
+
   return (
     <div className='space-y-8'>
-      <div className='flex items-center p-6 space-x-5 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500'>
+      <div className='flex items-center p-6 space-x-5 bg-white rounded-xl shadow-lg hover:shadow-xl'>
         <div className='flex bg-gray-100 p-4 w-full space-x-4 rounded-lg'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
@@ -22,15 +38,18 @@ export default function SearchBar({ bgColor = 'bg-red-600' }: Props) {
             />
           </svg>
           <input
-            className='bg-gray-100 outline-none'
+            className='bg-gray-100 outline-none w-full'
             type='text'
             placeholder='title or keyword...'
+            value={title}
+            onChange={handleChange}
           />
         </div>
         <div
+          onClick={handleClick}
           className={`${bgColor} py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer`}
         >
-          <span>Search</span>
+          Search
         </div>
       </div>
     </div>
