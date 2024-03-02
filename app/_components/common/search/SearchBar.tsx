5ba314576ff3e7659c9b83ab245928774bb33ca4
ayakuma309 <ayako.30901@gmail.com>
2024-03-02 23:50:27 +0900

    fix: bug

--- app/_components/common/search/SearchBar.tsx
+++ app/_components/common/search/SearchBar.tsx
@@ -31,9 +31,9 @@ export default function SearchBar({
             stroke='currentColor'
           >
             <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
               d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
             />
           </svg>
