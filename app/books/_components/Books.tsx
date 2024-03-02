ac7b9d26137d70e69e723d3d06deaae0701ba51a
ayakuma309 <ayako.30901@gmail.com>
2024-03-02 16:54:35 +0900

    add: search bar

--- app/books/_components/Books.tsx
+++ app/books/_components/Books.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Book from './Book';
 import { BookType } from '@/types/types';
+import SearchBar from '@/app/_components/common/search/SearchBar';
 
 type Props = {
   books: BookType[];
@@ -10,6 +11,7 @@ type Props = {
 export default function Books({ books }: Props) {
   return (
     <>
+      <SearchBar />
       <div className='flex flex-wrap items-center justify-center'>
         {books.map((book) => (
           <Book key={book.id} book={book} />
