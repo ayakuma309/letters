08351dfc6f6315e3b26358b38c6f6fee643cf75a
ayakuma309 <ayako.30901@gmail.com>
2024-03-02 15:32:51 +0900

    add: tags

--- app/_components/notion/Notion.tsx
+++ app/_components/notion/Notion.tsx
@@ -1,4 +1,5 @@
 import { getPageList } from '@/lib/notion';
+import Link from 'next/link';
 
 export async function NotionArticles() {
   const results = await getPageList();
@@ -7,20 +8,30 @@ export async function NotionArticles() {
       {results?.map((article, i) => (
         <div key={article.id}>
           {i + 1}
-          <a
-            className='flex h-full gap-x-6 rounded-xl bg-white px-4 py-3 ring-1 ring-gray-200 hover:bg-gray-50 md:px-6 md:shadow-sm dark:bg-gray-800 dark:hover:bg-gray-800/10'
+          <Link
+            className='flex flex-col h-full gap-x-6 rounded-xl bg-white px-4 py-3 ring-1 ring-gray-200 hover:bg-gray-50 md:px-6 md:shadow-sm dark:bg-gray-800 dark:hover:bg-gray-800/10'
             href={article.url as string}
             rel='noopener noreferrer'
             target='_blank'
           >
             <div className='line-clamp-3 flex flex-col pt-1'>
               <div className='flex flex-1 flex-col justify-between gap-2'>
-                <p className='line-clamp-2 font-bold text-gray-900 md:text-lg dark:text-gray-200'>
+                <p className='line-clamp-3 font-bold text-gray-900 md:text-lg dark:text-gray-200'>
                   {article.title}
                 </p>
               </div>
             </div>
-          </a>
+            <div className=''>
+              {article.tags.map((tag) => (
+                <div
+                  key={tag}
+                  className='text-xs text-gray-500 md:text-sm dark:text-gray-400'
+                >
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </Link>
         </div>
       ))}
     </div>
--- app/videos/all/page.tsx
+++ app/videos/all/page.tsx
@@ -1,9 +1,10 @@
 import { NotionArticles } from '@/app/_components/notion/Notion';
+import Loading from '@/app/loading';
 import { Suspense } from 'react';
 
 export default async function Page() {
   return (
-    <Suspense fallback={<p>Loading....</p>}>
+    <Suspense fallback={<Loading />}>
       <NotionArticles />
     </Suspense>
   );
