edd515510ca2ac87c20a8230130f3921b2b8b116
ayakuma309 <ayako.30901@gmail.com>
2024-03-03 14:46:47 +0900

    add: jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
